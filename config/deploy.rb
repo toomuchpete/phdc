79e965c63f1fb34006262922c6132aa9cec2fcec
Pete Holiday <pete.holiday@gmail.com>
2015-06-08 00:42:57 -0400

    Fix passenger deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :passenger_restart_with_sudo, true
+
 namespace :deploy do
 
   after :restart, :clear_cache do
