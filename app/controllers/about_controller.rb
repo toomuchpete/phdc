9b255ff4b5ac9b432bec6b148d8668ee17537546
Pete Holiday <pete.holiday@gmail.com>
2016-07-25 14:27:17 -0400

    Adding contact page.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,4 +2,8 @@ class AboutController < ApplicationController
     def cv
         @page_title = "What I've Done"
     end
+
+    def contact
+        @page_title = "Contact Me"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "homepage#index"
 
-  get '/cv', to: "about#cv", as: 'cv'
+  get '/cv', to: 'about#cv', as: 'cv'
+  get '/contact', to: 'about#contact', as: 'contact'
 end
